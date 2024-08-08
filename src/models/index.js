de35ede256c86d82d6ef13d91098c625fe026b06
chetansingade18 <chetansingade@gmail.com>
2024-08-08 23:03:45 +0530

    first commit

---
+++ src/models/index.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+try {
+    mongoose.connect(`mongodb://127.0.0.1:27017/ServicDesk`)
+    console.log("mongoose connected successfully")
+} catch (error) {
+    console.log(error)
+}
+
+export default mongoose
